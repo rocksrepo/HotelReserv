e81a64714f06f71b13062dc410487c1c3f2a03c0
Roshin <roshinhh@gmail.com>
2017-09-12 22:58:02 +0530

    fix db connection and create customer class

--- Hotel_Reservation_R2/src/db/DbConnect.java
+++ Hotel_Reservation_R2/src/db/DbConnect.java
@@ -1,9 +1,24 @@
 package db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnect {
 	private Connection conn;
+	private String uName = "root";
+	private String pass = "";
 	
-	
+	public DbConnect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/hotelReservation",uName,pass);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
---
+++ Hotel_Reservation_R2/src/main/Customer.java
@@ -0,0 +1,9 @@
+package main;
+
+import db.DbConnect;
+
+public class Customer {
+	public static void main(String[] argv) {
+		new DbConnect();
+	}
+}
